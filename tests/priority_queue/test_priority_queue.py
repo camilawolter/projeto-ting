d87a3726cb4e5485938400cc8274fa3b4673aa22
Camila Wolter <camila0wolter@gmail.com>
2023-05-08 15:44:06 -0300

    test: implementação do teste da classe PriorityQueue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,23 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+    priority_queue.enqueue({"qtd_linhas": 4})
+    priority_queue.enqueue({"qtd_linhas": 8})
+    priority_queue.enqueue({"qtd_linhas": 12})
+    priority_queue.enqueue({"qtd_linhas": 1})
+
+    assert len(priority_queue) == 4
+
+    assert priority_queue.search(0)["qtd_linhas"] == 4
+
+    priority_queue.dequeue()
+
+    assert priority_queue.search(0)["qtd_linhas"] == 1
+
+    assert priority_queue.search(1)["qtd_linhas"] == 8
+
+    with pytest.raises(IndexError, match="Índice Inválido"):
+        priority_queue.search(10)
