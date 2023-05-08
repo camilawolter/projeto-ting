1e80cfd83fa0501f750b7c65371016f686a61e0f
Camila Wolter <camila0wolter@gmail.com>
2023-05-08 15:16:34 -0300

    feat: implementação da função file_metadata

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,3 +1,4 @@
+import sys
 from ting_file_management.file_management import txt_importer
 
 
@@ -28,4 +29,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        file = instance.search(position)
+        print(file)
+    except IndexError:
+        sys.stderr.write("Posição inválida\n")
