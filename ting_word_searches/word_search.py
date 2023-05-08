378898c1994dd1b2db25f18cbee3b704bf181d90
Camila Wolter <camila0wolter@gmail.com>
2023-05-08 16:08:03 -0300

    feat: implementação da função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,26 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    files_with_word = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+        lines_word = []
+        line_number = 0
+
+        for line in file["linhas_do_arquivo"]:
+            line_number += 1
+
+            if word.lower() in line.lower():
+                lines_word.append({"linha": line_number})
+
+        if lines_word:
+            data = {
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": lines_word
+            }
+            files_with_word.append(data)
+
+    return files_with_word
 
 
 def search_by_word(word, instance):
