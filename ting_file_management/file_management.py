8d1719959a9476cf4293a5bfec5d21be7f47dc7a
Camila Wolter <camila0wolter@gmail.com>
2023-05-08 14:27:53 -0300

    feat: implementação da função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+from pathlib import Path
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+
+    file_patch = Path(path_file)
+    try:
+        return file_patch.read_text().splitlines()
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
